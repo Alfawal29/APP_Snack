3274c98b334d25e51ab440d2c7c9d339968ebf20
Ammar <ammar.alfawal@gmx.de>
2025-01-27 15:03:32 +0100

    Snack APP

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+
 import 'package:flutter_application_1/order_screen.dart';
 
 class StartScreen extends StatelessWidget {
